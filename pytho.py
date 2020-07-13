755ac367a484bd15037dd965a681539dde2947b4
prayash thapa <comppro54@gmail.com>
2020-07-13 07:33:23 +0545

    Adding firt python commit

---
+++ pytho.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+if a>b:
+    print("A is larger")
+elif b>a:
+    print("B is larger")
+else:
+    print("They are equal")    
+
+
